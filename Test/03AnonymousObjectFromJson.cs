53d83ddc5abd37fbd97e26954f040937f3ff4fad
nishant <nishant.singh87@gmail.com>
2021-03-08 21:14:36 +0530

    read json with newton soft

--- Test/03AnonymousObjectFromJson.cs
+++ Test/03AnonymousObjectFromJson.cs
@@ -25,5 +25,23 @@ namespace Tests
           Assert.AreEqual(json, response);
           Assert.AreEqual("value-my", response.tokenResponse.access_token);
         }
+        
+        public void ReadStringResponse()
+        {
+
+          var json = new
+          {
+                  time = "value",
+                  date = "good value",
+                  tokenResponse = new {
+                          access_token = "value-my"
+                  }
+          };
+          var jsonString = System.Text.Json.JsonSerializer.Serialize(json);
+          var response = Newtonsoft.Json.JsonConvert.DeserializeAnonymousType(jsonString, json);
+          
+          Assert.AreEqual(json, response);
+          Assert.AreEqual("value-my", response.tokenResponse.access_token);
+        }
     }
 }
